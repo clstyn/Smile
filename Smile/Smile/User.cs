90489f8c61ccec2cd940acb34e9e6c9dd39af04c
adityar22 <adityaramadhanarckarito47@gmail.com>
2022-09-11 21:52:52 +0700

    Add Project

---
+++ Smile/Smile/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smile
+{
+    public class User
+    {
+        //attribute here
+        public string usrName;
+        public string usrGender;
+        public DateTime usrBirthday;
+        public int usrAge;
+
+        //method here
+        public void displayProfile()
+        {
+
+        }
+
+        //constructor here
+        public User(string usrName, string usrGender, DateTime usrBirthday, int usrAge)
+        {
+            this.usrName = usrName;
+            this.usrGender = usrGender;
+            this.usrBirthday = usrBirthday;
+            this.usrAge = usrAge;
+        }
+        public User()
+        {
+
+        }
+    }
+}
---
+++ Smile/Smile/UserAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smile
+{
+    public class UserAccount: User
+    {
+        private string usrUname;
+        private string usrPassword;
+
+        public void credentialCheck()
+        {
+
+        }
+        public UserAccount(string usrUname, string usrPassword)
+        {
+            this.usrUname = usrUname;
+            this.usrPassword = usrPassword;
+        }
+        public string usrEmail_
+        {
+            set { this.usrUname = value; }
+        }
+        public string usrPassword_
+        {
+            set { this.usrPassword = value; }
+        }
+    }
+}
