46abb28f9ecebbb3ecc5df746d3c655c6e9c9892
adityar22 <adityaramadhanarckarito47@gmail.com>
2022-11-23 04:23:57 +0700

    add icon

--- Smile/Smile_Questionnaire.Designer.cs
+++ Smile/Smile_Questionnaire.Designer.cs
@@ -48,9 +48,10 @@ namespace Smile
             this.btnSmile.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnSmile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
             this.btnSmile.ForeColor = System.Drawing.Color.White;
-            this.btnSmile.Location = new System.Drawing.Point(14, 10);
+            this.btnSmile.Location = new System.Drawing.Point(10, 8);
+            this.btnSmile.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.btnSmile.Name = "btnSmile";
-            this.btnSmile.Size = new System.Drawing.Size(80, 29);
+            this.btnSmile.Size = new System.Drawing.Size(53, 20);
             this.btnSmile.TabIndex = 15;
             this.btnSmile.Text = "Smile";
             this.btnSmile.Click += new System.EventHandler(this.btnSmile_Click);
@@ -62,9 +63,10 @@ namespace Smile
             this.btnNext.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Bold);
             this.btnNext.ForeColor = System.Drawing.Color.White;
-            this.btnNext.Location = new System.Drawing.Point(742, 569);
+            this.btnNext.Location = new System.Drawing.Point(556, 462);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(47, 20);
+            this.btnNext.Size = new System.Drawing.Size(36, 15);
             this.btnNext.TabIndex = 17;
             this.btnNext.Text = "Next";
             this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
@@ -76,9 +78,10 @@ namespace Smile
             this.btnPrev.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnPrev.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Bold);
             this.btnPrev.ForeColor = System.Drawing.Color.White;
-            this.btnPrev.Location = new System.Drawing.Point(433, 569);
+            this.btnPrev.Location = new System.Drawing.Point(325, 462);
+            this.btnPrev.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.btnPrev.Name = "btnPrev";
-            this.btnPrev.Size = new System.Drawing.Size(47, 20);
+            this.btnPrev.Size = new System.Drawing.Size(35, 15);
             this.btnPrev.TabIndex = 18;
             this.btnPrev.Text = "Prev";
             this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
@@ -89,42 +92,47 @@ namespace Smile
             this.txtPage.BackColor = System.Drawing.Color.Transparent;
             this.txtPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
             this.txtPage.ForeColor = System.Drawing.Color.White;
-            this.txtPage.Location = new System.Drawing.Point(585, 569);
+            this.txtPage.Location = new System.Drawing.Point(439, 462);
+            this.txtPage.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.txtPage.Name = "txtPage";
-            this.txtPage.Size = new System.Drawing.Size(49, 29);
+            this.txtPage.Size = new System.Drawing.Size(34, 20);
             this.txtPage.TabIndex = 19;
             this.txtPage.Text = "1/4";
             // 
             // pageQuest1
             // 
             this.pageQuest1.BackColor = System.Drawing.Color.Transparent;
-            this.pageQuest1.Location = new System.Drawing.Point(309, -10);
+            this.pageQuest1.Location = new System.Drawing.Point(232, -8);
+            this.pageQuest1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pageQuest1.Name = "pageQuest1";
-            this.pageQuest1.Size = new System.Drawing.Size(617, 576);
+            this.pageQuest1.Size = new System.Drawing.Size(463, 468);
             this.pageQuest1.TabIndex = 20;
             // 
             // pageQuest2
             // 
             this.pageQuest2.BackColor = System.Drawing.Color.Transparent;
-            this.pageQuest2.Location = new System.Drawing.Point(309, -10);
+            this.pageQuest2.Location = new System.Drawing.Point(232, -8);
+            this.pageQuest2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pageQuest2.Name = "pageQuest2";
-            this.pageQuest2.Size = new System.Drawing.Size(617, 576);
+            this.pageQuest2.Size = new System.Drawing.Size(463, 468);
             this.pageQuest2.TabIndex = 21;
             // 
             // pageQuest3
             // 
             this.pageQuest3.BackColor = System.Drawing.Color.Transparent;
-            this.pageQuest3.Location = new System.Drawing.Point(309, -10);
+            this.pageQuest3.Location = new System.Drawing.Point(232, -8);
+            this.pageQuest3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pageQuest3.Name = "pageQuest3";
-            this.pageQuest3.Size = new System.Drawing.Size(617, 576);
+            this.pageQuest3.Size = new System.Drawing.Size(463, 468);
             this.pageQuest3.TabIndex = 22;
             // 
             // pageQuest4
             // 
             this.pageQuest4.BackColor = System.Drawing.Color.Transparent;
-            this.pageQuest4.Location = new System.Drawing.Point(309, -10);
+            this.pageQuest4.Location = new System.Drawing.Point(232, -8);
+            this.pageQuest4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pageQuest4.Name = "pageQuest4";
-            this.pageQuest4.Size = new System.Drawing.Size(617, 576);
+            this.pageQuest4.Size = new System.Drawing.Size(463, 468);
             this.pageQuest4.TabIndex = 23;
             // 
             // btnFinish
@@ -134,20 +142,21 @@ namespace Smile
             this.btnFinish.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnFinish.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Bold);
             this.btnFinish.ForeColor = System.Drawing.Color.White;
-            this.btnFinish.Location = new System.Drawing.Point(1088, 569);
+            this.btnFinish.Location = new System.Drawing.Point(816, 462);
+            this.btnFinish.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.btnFinish.Name = "btnFinish";
-            this.btnFinish.Size = new System.Drawing.Size(60, 20);
+            this.btnFinish.Size = new System.Drawing.Size(46, 15);
             this.btnFinish.TabIndex = 24;
             this.btnFinish.Text = "Finish";
             this.btnFinish.Click += new System.EventHandler(this.btnFinish_Click);
             // 
             // Smile_Questionnaire
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1216, 607);
+            this.ClientSize = new System.Drawing.Size(912, 493);
             this.Controls.Add(this.btnFinish);
             this.Controls.Add(this.txtPage);
             this.Controls.Add(this.btnPrev);
@@ -159,6 +168,7 @@ namespace Smile
             this.Controls.Add(this.pageQuest4);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Smile_Questionnaire";
